830405d569cfa9668b6f1007830a6024a6e0e6d3
Ignacio <ignacio_rnp@aol.com>
2017-10-11 19:40:27 +0000

    Classes
    The servos are controlled by keyboard input and the information is managed by  a class.
    I'm using heritance, polimorfism and a bunch of cool stuff here.
    It works.
    Kind of...

--- 09_10_2017.py
+++ 09_10_2017.py
@@ -7,9 +7,12 @@ import time
 import Adafruit_PCA9685 as PCA
 from Tkinter import *
 import random
+import abc		#Abstract class module
+from abc	import ABCMeta		
 
 f = 50		#frequency
-
+pwm = PCA.PCA9685()
+pwm.set_pwm_freq(f)
 # Declaration of  classes
 
 class Resolution:
@@ -17,27 +20,61 @@ class Resolution:
 	def __init__(self, mx, mn):
 		self.max = mx
 		self.min = mn
+		self.value = (self.max + self.min) / 2
+		
+	def check(self):
+		print("val:", self.value)
+		if self.value > self.max:
+			self.value = self.max
+			print("No es posible conseguir un valor superior")
+			
+		elif self.value < self.min:
+			self.value = self.min
+			print("No es posible conseguir un valor inferior")
+
 	
 class Servo:
 	"""Mother class for the different servos"""
+	
+	__metaclass__ = ABCMeta
 	def __init__(self, pin, name):
 		self.name = name
 		self.pin = pin
 		self.name = name
+		self.position = 45
 	
 	def showPos(self):
-		print (self.pos)
+		print (self.position)
 	
 	def showName(self):
 		print (self.name)
 		
+	@abc.abstractmethod
+	def move(self):
+		pass
+		
 
 class MG15(Servo):	
-	"""Class for the high torque servos 1501MG used for the neck movement"""
+	"""Class for the high torque servos 1501MG used for the neck movement
+	'm' and 'n' are the parameters of the angle-pwm linear function, which are different for each object"""
 	
-	def __init__(self, pin, name):
+	def __init__(self, pin, name, m, n):
 		Servo.__init__(self, pin, name)
 		self.res = 	Resolution(560, 100)
+		self.m = m
+		self.n = n			
+		
+	def move(self):
+		pwm.set_pwm(self.pin, self.pin, self.position) 
+		
+	def pos(self, angle):
+		if angle < 100 and angle > 20:
+			self.position = self.m * angle + self.n
+			self.res.value = self.pos 
+			self.res.check()
+		else:
+			pass
+		
 
 class S3114:
 	"""Class for the Futaba micro servos used in the eyes"""
@@ -46,36 +83,41 @@ class S3114:
 		Servo.__init__(self, pin, name)
 		self.res = 	Resolution(4096, 0)
 	
+	def move(self):
+		pwm.set_pwm(self.pin, self.pin, self.pos) 
+
+alpha = MG15(0, "Alpha", -3, 540) 
+bravo = MG15(1, "Bravo", 3, 100)
+charile = MG15(2, "Charlie", -3, 540)
+
+
 def update_servos():
-
-	#Vertical update
-	ver = bottom + (bottom - top) * ver_slider_value.get() / 100
-	up(ver, ver)
-			#Horizontal update
-	hor = servo_min + (servo_max - servo_min) * hor_slider_value.get() / 100 
-	turn(hor)
-			#Update frequency
-	root.after(50, update_servos)
 	
-	
-alpha, bravo, charlie = MG15(0, "Alpha"), MG15(1, "Bravo"), MG15(2, "Charlie")
+	a = input("Test value for alpha")
+	alpha.pos(a)
+	alpha.move()
+	bravo.move()
+	charlie.move()
 
-pwm = PCA.PCA9685()
-pwm.set_pwm_freq(f)
+#root.after(50, update_servos)
 
+a = input("Test value for alpha")
+b = input("Test value for bravo")
+alpha.pos(a)
+bravo.pos(b)
+alpha.move()
+bravo.move()
+
+
+
+"""
 root = Tk()
 frame = Frame(root)
 frame.pack()
 
-sliderV = Scale(root, from_ = 100, to= 0, orient=VERTICAL, variable = ver_slider_value)
-sliderV.pack()
-
-sliderH = Scale(root, from_ = 0, to=100, orient=HORIZONTAL, variable = hor_slider_value)
-sliderH.pack()
-	
 root.after(1000, update_servos)
 root.mainloop()
 
 
-
+"""
 
